33edead68e2c47c23000e952a99a70cc45a9e549
Stoyan Haydushki <haydushki@uni-sofia.bg>
2024-02-07 21:23:27 +0200

    Add some functions and fields to Canvas class

--- src/app/Canvas.cpp
+++ src/app/Canvas.cpp
@@ -1,3 +1,51 @@
 #include "Canvas.hpp"
 
-Canvas::Canvas(QWidget *parent) : QWidget(parent) {}
+#include <utility>
+
+Canvas::Canvas(QWidget *parent) : QWidget(parent) {
+    setMouseTracking(true);
+}
+
+void Canvas::setPointModel(std::shared_ptr<PointModel> model) {
+    this->pointModel = std::move(model);
+
+    connect(this->pointModel.get(), &PointModel::dataChanged, this, &Canvas::recalculateAndUpdate);
+    connect(this->pointModel.get(), &PointModel::rowsInserted, this, &Canvas::recalculateAndUpdate);
+    connect(this->pointModel.get(), &PointModel::rowsRemoved, this, &Canvas::recalculateAndUpdate);
+    update();
+}
+
+void Canvas::clear() {
+    // TODO: Implement
+}
+
+void Canvas::mousePressEvent(QMouseEvent *event) {
+    if (event->button() == Qt::MouseButton::LeftButton) {
+        emit pointAdded(Point(event->pos().x(), event->pos().y(), 1));
+    }
+    QWidget::mousePressEvent(event);
+}
+
+void Canvas::paintEvent(QPaintEvent *event) {
+    QWidget::paintEvent(event);
+}
+
+void Canvas::drawPoints(QPainter &painter) {
+    // TODO: Implement
+}
+
+void Canvas::drawTopLevelLines(QPainter &painter) {
+    // TODO: Implement
+}
+
+void Canvas::drawBezierCurve(QPainter &painter, qreal progress) {
+    // TODO: Implement
+}
+
+void Canvas::updateConfiguration() {
+    // TODO: Implement
+}
+
+void Canvas::recalculateAndUpdate() {
+    // TODO: Implement
+}
--- src/app/Canvas.hpp
+++ src/app/Canvas.hpp
@@ -3,13 +3,58 @@
 
 
 #include <QWidget>
+#include <QPen>
+#include <QMouseEvent>
+#include "curve_lib/RationalBezierPoint.hpp"
+#include "models/PointModel.hpp"
 
 class Canvas : public QWidget {
-    Q_OBJECT
+Q_OBJECT
 
 public:
-    explicit Canvas(QWidget* parent);
+    explicit Canvas(QWidget *parent = nullptr);
 
+    void setPointModel(std::shared_ptr<PointModel> model);
+
+    virtual ~Canvas() = default;
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+
+    void paintEvent(QPaintEvent *event) override;
+
+public slots:
+
+    void clear();
+
+signals:
+
+    void pointAdded(CurveLib::RationalBezierPoint);
+
+private:
+    void drawPoints(QPainter &painter);
+
+    void drawTopLevelLines(QPainter &painter);
+
+    void drawBezierCurve(QPainter &painter, qreal progress = 1.0);
+
+    void updateConfiguration();
+
+    void recalculateAndUpdate();
+
+    std::vector<Point> calculatedBezierPoints;
+    bool bezierCurveNeedsRecalculation;
+
+    std::shared_ptr<PointModel> pointModel;
+    qreal pointRadius;
+
+    bool drawPointsToggle;
+    bool connectPointsToggle;
+    bool drawBezierCurveToggle;
+    QPen pointPen;
+    QBrush pointBrush;
+    QPen lineBetweenPointsPen;
+    QPen bezierCurvePen;
 };
 
 
