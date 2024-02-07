55a4cba418dab57ce2294cd76508d450d7eeb291
Stoyan Haydushki <haydushki@uni-sofia.bg>
2024-02-07 21:14:43 +0200

    Add PointListView

---
+++ src/app/views/PointListView.cpp
@@ -0,0 +1,10 @@
+#include "PointListView.hpp"
+
+PointListView::PointListView(QWidget *parent) : QListView(parent) {}
+
+void PointListView::keyPressEvent(QKeyEvent *event) {
+    if (event->key() == Qt::Key_Delete) {
+        const int row = currentIndex().row();
+        model()->removeRow(row); // TODO: This doesn't work atm
+    }
+}
---
+++ src/app/views/PointListView.hpp
@@ -0,0 +1,17 @@
+#ifndef CAGD_RATIONAL_BEZIER_POINTLISTVIEW_HPP
+#define CAGD_RATIONAL_BEZIER_POINTLISTVIEW_HPP
+
+
+#include <QListView>
+#include <QKeyEvent>
+
+class PointListView : public QListView {
+public:
+    explicit PointListView(QWidget *parent = nullptr);
+
+protected:
+    void keyPressEvent(QKeyEvent *event);
+};
+
+
+#endif //CAGD_RATIONAL_BEZIER_POINTLISTVIEW_HPP
